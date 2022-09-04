6725fc94c839e2489dd06fea80006d4202461205
Angkush Tarachand <angkushtarachand@gmail.com>
2022-09-04 22:20:22 +0400

    database

---
+++ Hackathon_2/app/forms.py
@@ -0,0 +1,15 @@
+import flask_wtf
+import wtforms
+
+
+class Login(flask_wtf.Flaskform):
+    first_name = wtforms.StringField("First Name")
+    last_name = wtforms.StringField("Last Name")
+    email = wtforms.StringField("Email")
+    password = wtforms.PasswordField("Password")
+    confirm_password = wtforms.PasswordField("Confirm Password")
+
+    submit = wtforms.SubmitField("Submit")
+
+
+
